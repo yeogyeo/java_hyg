ecca9ce3f20f665a0de486c7c3d73be781fe9b7f
KOREAVC <KOREAVC@DESKTOP-588IM88>
2023-08-31 18:15:07 +0900

    8월 31일 수업내용 변수,연산자

---
+++ p0831/src/p0831/JA0831_10.java
@@ -0,0 +1,23 @@
+package p0831;
+
+import java.util.Scanner;
+
+public class JA0831_10 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("아이디를 입력하세요.");
+		  String id = scan.next(); //next(),nextLine():문자열, nextInt:정수, nextDouble():실수
+		  
+		  System.
+		  
+		
+	
+       
+	
+       
+		
+		
+	}
+
+}
